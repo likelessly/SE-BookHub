f2cc3a6dd42223686b48e641655a4f879f798f07
OakJkpG <jakapong2547@gmail.com>
2025-03-09 04:53:24 +0700

    Delete temp

--- pages/db_config.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-MONGO_URI = "mongodb+srv://OakJkpG:awZfdwaIMPNiHPNP@cluster0.aeia6.mongodb.net/"
-client = MongoClient(MONGO_URI)
-db = client["BookHub_DB"]
-book_collection = db["books"]
-user_collection = db["users"]
--- pages/models.py
+++
@@ -1,8 +0,0 @@
-from djongo import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    image_url = models.URLField()
-
-
