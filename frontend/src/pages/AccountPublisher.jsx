bda79a82b1639146f85216bb62103cb2e9088a44
OakJkpG <jakapong2547@gmail.com>
2025-03-29 00:52:02 +0700

    fix some edit book

--- frontend/src/pages/AccountPublisher.jsx
+++ frontend/src/pages/AccountPublisher.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Account.css';
 import { useNavigate } from 'react-router-dom';
-import { uploadImage, uploadPDF } from '../api';
+import { uploadImage} from '../api';
 
 const AccountPublisher = () => {
   const [accountData, setAccountData] = useState(null);
