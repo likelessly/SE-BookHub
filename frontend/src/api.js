8e9427de848e5c2af1a85e592aaa65d790ab3434
likelessly <omegalul101@hotmail.com>
2025-04-02 06:29:12 +0700

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import axios from 'axios';
 
 const supabaseUrl = 'https://csqtsflaklabqsnjlioy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzcXRzZmxha2xhYnFzbmpsaW95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1NDYxMTMsImV4cCI6MjA1NzEyMjExM30.FtAAPtRd4at3nrkRIPL-OM9pBwnLyddvqf1fY84ZF7k';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzcXRzZmxha2xhYnFzbmpsaW95Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MTU0NjExMywiZXhwIjoyMDU3MTIyMTEzfQ.5ShmiI7t-b8LTxKeh3TxE0RILSdG5Ss6a1WrkaxqaAI';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
